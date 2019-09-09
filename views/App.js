bf0f9e34260855bafa2055e6a92d1c888de3a8bb
kano233333 <1320395666@qq.com>
2019-09-09 22:22:01 +0800

    < first

---
+++ views/App.js
@@ -0,0 +1,5 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+
+ReactDOM.render(<h2>aaaa</h2>,document.getElementById('root'));
---
+++ webpack.config.js
@@ -0,0 +1,42 @@
+const webpack = require('webpack');
+const path = require('path');
+const htmlWebpackPlugin = require("html-webpack-plugin");
+const ExtractTextPlugin = require("extract-text-webpack-plugin");
+  
+module.exports = {
+  entry: {
+    entry: './views/App.js'
+  },
+  output: {
+    path: path.resolve(__dirname, './public/dist'),
+    filename: "bundle.js"
+  },
+  resolve: {
+    extensions: ['.js', '.jsx','.css']
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js|\.jsx$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel-loader',
+        query:{
+          presets:['react','es2015']
+        }
+      },
+      {
+        test: /\.sass|\.scss$/,
+        exclude: /node_modules/,
+        use: ['style-loader', 'css-loader', 'sass-loader']
+      }
+    ]
+  },
+  plugins: [
+    new htmlWebpackPlugin({
+      filename: "index.html",
+      template: "index.html",
+      inject: "body"
+    }),
+    // new ExtractTextPlugin("/public/dist/stylesheets/main.css")
+  ] 
+}
