5138147f15cbcbe915d0b811f862238b02eb2124
kano233333 <1320395666@qq.com>
2019-09-15 22:01:35 +0800

    ...

--- public/src/javascripts/const.js
+++ public/src/javascripts/const.js
@@ -25,86 +25,86 @@ const monthData = [
   {
     en:'January',
     zh:'一月',
-    _zh:'',
+    _zh:'水仙',
     color:'#f1c4cd',
-    font_color:''
+    font_color:'#fff'
   },
   {
     en:'February',
     zh:'二月',
     _zh:'杏',
     color:'#f0a1a8',
-    font_color:''
+    font_color:'#fff'
   },
   {
     en:'March',
     zh:'三月',
     _zh:'桃',
     color:'#f07c82',
-    font_color:''
+    font_color:'#fff'
   },
   {
     en:'April',
     zh:'四月',
     _zh:'槐',
     color:'#55bb8a',
-    font_color:''
+    font_color:'#fff'
   },
   {
     en:'May',
     zh:'五月',
     _zh:'榴',
     color:'#20a162',
-    font_color:''
+    font_color:'#fff'
   },
   {
     en:'June',
     zh:'六月',
     _zh:'荷',
     color:'#1a6840',
-    font_color:''
+    font_color:'#fff'
   },
   {
     en:'July',
     zh:'七月',
-    _zh:'#f8df70',
+    _zh:'栀子',
     color:'#eed045',
-    font_color:''
+    font_color:'#fff'
   },
   {
     en:'August',
-    zh:'',
-    _zh:'',
+    zh:'八月',
+    _zh:'桂',
     color:'#ffd111',
-    font_color:''
+    font_color:'#fff'
   },
   {
     en:'September',
-    zh:'',
-    _zh:'',
+    zh:'九月',
+    _zh:'菊',
     color:'#f8df70',
-    font_color:''
+    font_color:'#fff'
   },
   {
     en:'October',
-    zh:'',
-    _zh:'',
+    zh:'十月',
+    _zh:'芙蓉',
     color:'#dad4cb',
-    font_color:''
+    font_color:'#333'
   },
   {
     en:'November',
-    zh:'',
-    _zh:'',
+    zh:'十一月',
+    _zh:'山茶',
     color:'#e4dfd7',
-    font_color:''
+    font_color:'#333'
   },
   {
     en:'December',
-    zh:'',
-    _zh:'',
+    zh:'十二月',
+    _zh:'梅',
     color:'#f8f4ed',
-    font_color:''
+    font_color:'#333'
   }
 ]
 export { nav }
